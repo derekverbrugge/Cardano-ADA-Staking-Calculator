ca97d21f08e5e025affc3af198d6b17e8b445634
Derek Verbrugge <52356885+derekverbrugge@users.noreply.github.com>
2023-10-26 05:56:09 -0400

    Update AdaCalc.py
    formatting improvements

--- AdaCalc.py
+++ AdaCalc.py
@@ -22,21 +22,38 @@ if adaprice == 0 or adaprice == 1:
     adaorusd = "ADA"
     dollarsym = ""
     usdmult = 1
+    if adaprice == 1:
+        adaorusd = "ADA/USD"
 else:
     adaorusd = "USD"
     dollarsym = "$"
     usdmult = adaprice
+
     
 #print results
-print(f"Day: {dollarsym}{((year_reward / 365) * min) * usdmult:.2f} {adaorusd} - {dollarsym}{((year_reward / 365) * max) * usdmult:.2f} {adaorusd} (average of {dollarsym}{(year_reward / 365) * usdmult:.2f} {adaorusd})")
-print(f"Epoch: {dollarsym}{((year_reward / epochs) * min) * usdmult:.2f} {adaorusd} - {dollarsym}{((year_reward / epochs) * max) * usdmult:.2f} {adaorusd} (average of {dollarsym}{(year_reward / epochs) * usdmult:.2f} {adaorusd})")
-print(f"Month: {dollarsym}{((year_reward / 12) * min) * usdmult:.2f} {adaorusd} - {dollarsym}{((year_reward / 12) * max) * usdmult:.2f} {adaorusd} (average of {dollarsym}{(year_reward / 12) * usdmult:.2f} {adaorusd})")
-print(f"Year: {dollarsym}{(year_reward * min) * usdmult:.2f} {adaorusd} - {dollarsym}{(year_reward * max) * usdmult:.2f} {adaorusd} (average of {dollarsym}{(year_reward) * usdmult:.2f} {adaorusd}) ") 
-print(f"Total accumulated after 1 year: {dollarsym}{(principle + year_reward) * usdmult:.2f} {adaorusd} | {dollarsym}{(principle + year_reward * min) * usdmult:.2f} {adaorusd} - {dollarsym}{(principle + year_reward * max) * usdmult:.2f} {adaorusd} ")
+print("")
+if adaprice == 0 or adaprice == 1:
+    print(f"Rewards on {principle:.2f} {adaorusd} for the 1st year.")
+else:
+    print(f"Rewards on {principle:.2f} ADA or {dollarsym}{(adaprice * principle):.2f} USD @ {dollarsym}{adaprice:.2f} USD/ADA for the 1st year.")
+print(f"Day:    {dollarsym}{(year_reward / 365) * usdmult:.2f} {adaorusd} ({dollarsym}{((year_reward / 365) * min) * usdmult:.2f} {adaorusd} - {dollarsym}{((year_reward / 365) * max) * usdmult:.2f} {adaorusd})")
+print(f"Epoch:  {dollarsym}{(year_reward / epochs) * usdmult:.2f} {adaorusd} ({dollarsym}{((year_reward / epochs) * min) * usdmult:.2f} {adaorusd} - {dollarsym}{((year_reward / epochs) * max) * usdmult:.2f} {adaorusd})")
+print(f"Month:  {dollarsym}{(year_reward / 12) * usdmult:.2f} {adaorusd} ({dollarsym}{((year_reward / 12) * min) * usdmult:.2f} {adaorusd} - {dollarsym}{((year_reward / 12) * max) * usdmult:.2f} {adaorusd})")
+print(f"Year:   {dollarsym}{(year_reward) * usdmult:.2f} {adaorusd} ({dollarsym}{(year_reward * min) * usdmult:.2f} {adaorusd} - {dollarsym}{(year_reward * max) * usdmult:.2f} {adaorusd}) ") 
+if years >= 1:
+    if years > 9: 
+        space = ' '
+        if years > 99: 
+            space = '  '
+    else: space = ''
+    print("")
+    
+    print(f"Total accumulated up to {years} years.")
+    print(f"{space}1 year:  {dollarsym}{(principle + year_reward) * usdmult:.2f} {adaorusd} | ({dollarsym}{(principle + year_reward * min) * usdmult:.2f} {adaorusd} - {dollarsym}{(principle + year_reward * max) * usdmult:.2f} {adaorusd})")
 if years >= 5:
-    print(f"Total accumulated after 5 years: {dollarsym}{(fiveyreward) * usdmult:.2f} {adaorusd} | {dollarsym}{(fiveyreward * min) * usdmult:.2f} {adaorusd} - {dollarsym}{(fiveyreward * max) * usdmult:.2f} {adaorusd}  ")
+    print(f"{space}5 years: {dollarsym}{(fiveyreward) * usdmult:.2f} {adaorusd} | ({dollarsym}{(fiveyreward * min) * usdmult:.2f} {adaorusd} - {dollarsym}{(fiveyreward * max) * usdmult:.2f} {adaorusd})")
 if 1 < years != 5:
-    print(f"Total accumulated after {years} years: {dollarsym}{(reward) * usdmult:.2f} {adaorusd} | {dollarsym}{(reward * min) * usdmult:.2f} {adaorusd} - {dollarsym}{(reward * max) * usdmult:.2f} {adaorusd} ")
+    print(f"{years} years: {dollarsym}{(reward) * usdmult:.2f} {adaorusd} | ({dollarsym}{(reward * min) * usdmult:.2f} {adaorusd} - {dollarsym}{(reward * max) * usdmult:.2f} {adaorusd})")
 
     
     
