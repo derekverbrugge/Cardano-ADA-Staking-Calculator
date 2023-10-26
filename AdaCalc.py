07f4f90ce6abf574fb8c615c0273e5d9442fda7a
Derek Verbrugge <52356885+derekverbrugge@users.noreply.github.com>
2023-10-26 03:26:36 -0400

    Update AdaCalc.py
    added comments

--- AdaCalc.py
+++ AdaCalc.py
@@ -1,15 +1,23 @@
-principle = float(input("Enter the amount of ADA you want to stake: "))
-years = int(input("How many years should be calculated: "))
+
+# Inspired by the adatainment ada staking calculator @ https://www.adatainment.com/index.php?page=staking_calculator 
+# Based on IOHK's literature @ https://iohk.io/en/research/library/#reward-sharing-schemes-for-stake-pools
+
+principle = float(input("Enter the amount of ADA you want to stake: ")) #initial investment amount in ADA
+years = int(input("How many years should be calculated: ")) #number of years staking rewards will be calculated to.
 adaprice = float(input("Enter your target price of ADA in USD to estimate in USD or enter 1 or 0 to calculate in ADA: "))
-rate = 0.05
-epochs = 73 #this is the compounding frequency ADA epochs are roughly 5 days
-min = .9262
+rate = 0.05 #staking rewards
+epochs = 73 #this is the compounding frequency. ADA epochs are roughly 5 days.
+
+#min and max represent variance depending on the staking pool.
+
+min = .9262 
 max = 1.1926
 
-reward = principle * (1 + (rate / epochs))**(epochs * years)
-fiveyreward = principle * (1 + (rate / epochs))**(epochs * 5)
-year_reward = (principle * rate)
+reward = principle * (1 + (rate / epochs))**(epochs * years) #reward calculated with compounding interest.
+fiveyreward = principle * (1 + (rate / epochs))**(epochs * 5) #five year reward calculated with compounding interest.
+year_reward = (principle * rate) #reward for a year.
 
+#change symbols and denomination and apply multiplier for USD as necessary.
 if adaprice == 0 or adaprice == 1: 
     adaorusd = "ADA"
     dollarsym = ""
@@ -19,7 +27,7 @@ else:
     dollarsym = "$"
     usdmult = adaprice
     
-
+#print results
 print(f"Day: {dollarsym}{((year_reward / 365) * min) * usdmult:.2f} {adaorusd} - {dollarsym}{((year_reward / 365) * max) * usdmult:.2f} {adaorusd} (average of {dollarsym}{(year_reward / 365) * usdmult:.2f} {adaorusd})")
 print(f"Epoch: {dollarsym}{((year_reward / epochs) * min) * usdmult:.2f} {adaorusd} - {dollarsym}{((year_reward / epochs) * max) * usdmult:.2f} {adaorusd} (average of {dollarsym}{(year_reward / epochs) * usdmult:.2f} {adaorusd})")
 print(f"Month: {dollarsym}{((year_reward / 12) * min) * usdmult:.2f} {adaorusd} - {dollarsym}{((year_reward / 12) * max) * usdmult:.2f} {adaorusd} (average of {dollarsym}{(year_reward / 12) * usdmult:.2f} {adaorusd})")
